48366e7d16df54b52a48ae41c06dfe75fa4dcd24
mbordon1 <martibordon31@gmail.com>
2026-05-05 11:38:52 -0300

    funciones post y put

--- clase05-05_APIREST/funciones.mjs
+++ clase05-05_APIREST/funciones.mjs
@@ -23,11 +23,35 @@ export function obtenerProductoPorId(req, res) {
 }
 
 export function crearProducto(req, res) {
-
+    const nuevoProducto = req.body;
+        nuevoProducto.id = productos.length + 1;    
+    productos.push(nuevoProducto);
+    const respuesta = {
+        datos: nuevoProducto,
+        url: 'http://localhost:3000/api/v1/productos/' + nuevoProducto.id,
+        status: 201,
+        mensaje: 'Producto creado'
+    }
+    res.json(respuesta);
 }
 
 export function actualizarProducto(req, res) {
-
+    const id_producto = Number(req.params.id);
+    const index = productos.findIndex((producto) => Number(producto.id) === id_producto);   
+    if (index !== -1) {
+        const productoActualizado = req.body;
+        productoActualizado.id = id_producto;
+        productos[index] = productoActualizado;
+        const respuesta = {
+            datos: productoActualizado,
+            url: 'http://localhost:3000/api/v1/productos/' + id_producto,
+            status: 200,
+            mensaje: 'Producto actualizado'
+        }
+        res.json(respuesta);
+    } else {
+        res.status(404).json({mensaje: 'Producto no encontrado'})
+    }
 }
 
 export function eliminarProducto(req, res) {
